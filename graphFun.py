0ba0563c3daa220fcc16c623bfd41bbfaaeb9f4c
Ron Cordell <ron.cordell@berkeley.edu>
2015-07-31 18:33:00 -0700

    Adding recent Python code for graphs and other experiments

---
+++ graphFun.py
@@ -0,0 +1,101 @@
+'''
+Read a file that contains an adjacency matrix and construct a node-based graph.
+
+The adjacency matrix file, adj.txt, contains the following:
+
+
+0 0 0 1 1 0 1 0 1 0 1 0 0 0 0 1 1 0 1 0 1 1 0 1 1  
+0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 1 1  
+0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0  
+1 1 0 0 0 1 0 0 1 1 0 1 0 0 0 1 0 0 0 0 1 1 0 1 1  
+1 1 0 0 0 0 1 1 1 0 0 0 1 0 0 1 0 0 0 0 1 1 1 0 0  
+0 0 0 1 0 0 1 0 1 0 0 1 0 0 0 0 0 0 0 1 0 0 0 1 0  
+1 0 0 0 1 1 0 1 0 0 0 0 1 0 0 1 0 1 0 0 0 0 0 0 0  
+0 0 1 0 1 0 1 0 1 0 0 1 1 1 0 1 0 0 1 0 0 0 0 1 0  
+1 0 0 1 1 1 0 1 0 0 0 0 0 0 0 0 1 0 1 0 1 1 1 0 1  
+0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0 0 0 0 1 0  
+1 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 1 1 1 0 0  
+0 0 0 1 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0  
+0 0 0 0 1 0 1 1 0 0 1 0 0 1 0 0 1 1 0 1 0 0 1 0 0  
+0 0 0 0 0 0 0 1 0 0 1 0 1 0 1 0 1 0 0 1 0 0 0 0 0  
+0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0 0 0 0 0 1 0 0  
+1 0 0 1 1 0 1 1 0 1 0 0 0 0 1 0 1 0 1 1 1 0 1 1 0  
+1 0 0 0 0 0 0 0 1 1 0 0 1 1 0 1 0 0 0 0 0 1 0 1 0  
+0 1 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 1 0 1 0 0 0  
+1 0 0 0 0 0 0 1 1 1 0 0 0 0 0 1 0 1 0 0 1 1 0 1 0  
+0 0 0 0 0 1 0 0 0 0 0 1 1 1 0 1 0 1 0 0 1 0 1 1 0  
+1 0 0 1 1 0 0 0 1 0 1 0 0 0 0 1 0 0 1 1 0 0 0 0 0  
+1 0 0 1 1 0 0 0 1 0 1 0 0 0 0 0 1 1 1 0 0 0 0 1 1  
+0 1 1 0 1 0 0 0 1 0 1 1 1 0 1 1 0 0 0 1 0 0 0 0 0  
+1 1 0 1 0 1 0 1 0 1 0 0 0 0 0 1 1 0 1 1 0 1 0 0 0  
+1 1 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+
+'''
+
+
+from graphnode import GraphNode 
+from Queue import Queue
+
+# breadth-first search algorithm
+# graph is a dictionary of nodes such that name : node
+# node is the name or value of the node
+
+def bfs(graph, node):
+	levels = {}
+
+	d = []
+	q = Queue()
+	q.put((graph[node],0))
+	d.append(graph[node])
+
+	while not q.empty():
+		v,l = q.get()
+		if l not in levels:
+			levels[l] = []
+		levels[l].append(v.value)
+		for connection in v.get_connections():
+			if connection not in d:
+				q.put((connection, l+1))
+				d.append(connection)
+	return levels
+
+# n1 and n2 are the node values or names
+# g is the graph dictionary where n1 and n2 are part of the keys
+
+def equidistant_nodes(g, n1, n2):
+	l0 = bfs(g, n1)
+	l1 = bfs(g, n2)
+
+	eq = []
+	for key in l0.keys():
+		for node in l0[key]:
+			if key in l1.keys():
+				if node in l1[key]:
+					eq.append(node)
+	eq.sort()			
+	return eq
+
+def read_adj_file(fname):
+	graphnodes = {}
+	with open(fname) as f:
+		row = 0
+		for line in f:
+			col = 0
+			for bit in line.split():
+				if bit == '1':
+					if col not in graphnodes.keys():
+						graphnodes[col] = GraphNode(col)
+					if row not in graphnodes.keys():
+						graphnodes[row] = GraphNode(row)
+					graphnodes[col].add_connections(graphnodes[row])
+				col = col + 1
+			row = row + 1
+	return graphnodes	
+
+if __name__ == '__main__':
+	graphnodes = read_adj_file('adj.txt')
+
+	print '0 and 1 : ' + str(len(equidistant_nodes(graphnodes, 0, 1)))
+	print '0 and 5 : ' + str(len(equidistant_nodes(graphnodes, 0, 5)))
+	print '1 and 8 : ' + str(len(equidistant_nodes(graphnodes, 1, 8)))
+
